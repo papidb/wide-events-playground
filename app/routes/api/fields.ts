0b2644fe28b2b840ead71c2a27d7c69e0b0b85fa
Daniel <benjamindaniel706@gmail.com>
2024-11-19 13:32:22 +0100

    chore: init tanstack start

---
+++ app/routes/api/fields.ts
@@ -0,0 +1,19 @@
+import { json } from "@tanstack/start";
+import { createAPIFileRoute } from "@tanstack/start/api";
+import {
+  groupByTableName,
+  transformRowDataToFieldData,
+} from "../../../src/data";
+import getDatabase from "../../../src/db";
+
+export const Route = createAPIFileRoute("/api/fields")({
+  GET: async ({ request, params }) => {
+    const db = await getDatabase();
+    const tables = (
+      await db.all(
+        "SELECT column_name, data_type, table_name FROM information_schema.columns"
+      )
+    ).map(transformRowDataToFieldData);
+    return json(groupByTableName(tables));
+  },
+});
--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,15 @@
 import { Database } from "duckdb-async";
 
-export default async function setupConnection() {
-  let db = await Database.create("http_logs.duckdb");
-
+let db: Database;
+export default async function getDatabase() {
+  if (db) return db;
+  db = await Database.create("http_logs.duckdb");
   return db;
 }
+
+// hacky fix for bigInt
+// https://github.com/GoogleChromeLabs/jsbi/issues/30#issuecomment-953187833
+// @ts-expect-error no toJSON method
+BigInt.prototype.toJSON = function () {
+  return this.toString();
+};
